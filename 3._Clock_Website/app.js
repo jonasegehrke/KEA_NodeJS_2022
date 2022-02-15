3edb7d052408fafa2432ef85464d5d2165bf1080
Jonas Gehrke <jonasegherke@gmail.com>
2022-02-15 13:09:30 +0100

    Fixing heroku

--- 3._Clock_Website/app.js
+++ 3._Clock_Website/app.js
@@ -8,6 +8,6 @@ app.get("/index",(req, res)=>{
     res.sendFile(__dirname + "/public/index.html")
 })
 
-app.listen(8080,()=>{
-    console.log("Server is running on port", 8080)
-})
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
