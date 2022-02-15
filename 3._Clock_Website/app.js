1adfc5e5634d7b4592624f83cdba7260a21c3bfb
Jonas Gehrke <jonasegherke@gmail.com>
2022-02-15 13:04:07 +0100

    trying to fix heroku

--- 3._Clock_Website/app.js
+++ 3._Clock_Website/app.js
@@ -4,7 +4,7 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get("/",(req, res)=>{
+app.get("/index",(req, res)=>{
     res.sendFile(__dirname + "/public/index.html")
 })
 
